35829dbd6fc4b795e984002f08f7ebbde513a951
megumi-ben <163231066+megumi-ben@users.noreply.github.com>
2024-06-05 10:51:51 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        FriendshipGraph graph=new FriendshipGraph();
+        Person rachel=new Person("Rachel");
+        Person ben=new Person("Ben");
+        Person ross=new Person("Ross");
+        Person kei=new Person("Kei");
+        graph.addVertex(rachel);
+        graph.addVertex(ben);
+        graph.addVertex(ross);
+        graph.addVertex(kei);
+        graph.addEdge(rachel,ross);
+        graph.addEdge(ross,rachel);
+        graph.addEdge(ben,ross);
+        graph.addEdge(ross,ben);
+        System.out.println(graph.getDistance(rachel,ross));
+        System.out.println(graph.getDistance(rachel,ben));
+        System.out.println(graph.getDistance(rachel,rachel));
+        System.out.println(graph.getDistance(rachel,kei));
+    }
+}
---
+++ Person.java
@@ -0,0 +1,30 @@
+public class Person {
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Person{name = " + name + "}";
+    }
+}
